11b46fe48ad909d666e16ffc7086b627e3e0126d
E-Elements-tw <jin.chen@e-elememts.com.tw>
2018-11-20 16:32:19 +0800

    .

--- E_Elements_labs/__init__.py
+++ E_Elements_labs/__init__.py
@@ -1,10 +1,9 @@
 from .loop import TestOverlay
 from .pitch_shift import pitch
-from . import cv2pynq
+
 
 __author__ = "E-Elements"
 __copyright__ = "Copyright 2017, Xilinx"
-__all__ = ['lib']
 __version__ = 0.6
 
 
