56aea7c29d78ee6657ad9caf4062d40486d78fa0
E-Elements-tw <jin.chen@e-elememts.com.tw>
2018-11-20 14:32:07 +0800

    .

--- E_Elements_labs/cv2pynq.py
+++ E_Elements_labs/cv2pynq.py
@@ -6,7 +6,7 @@ from pynq.lib import DMA
 import cv2
 
 #CV2PYNQ_ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
-CV2PYNQ_BIT_DIR = os.path.join('/home/xilinx/E-elements_lab', 'bitstreams')
+CV2PYNQ_BIT_DIR = os.path.join('/home/xilinx/E_Elements_labs', 'bitstreams')
 
 class cv2pynq():
     MAX_WIDTH  = 1920
