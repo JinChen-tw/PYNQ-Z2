39089f4df6969d37a2433cfa35b2d0967f387bcd
E-Elements-tw <jin.chen@e-elememts.com.tw>
2018-11-21 09:34:33 +0800

    .

--- E_Elements_labs/__init__.py
+++ E_Elements_labs/__init__.py
@@ -3,8 +3,8 @@ from .pitch_shift import pitch
 from . import cv2pynq
 
 __author__ = "E-Elements"
-__copyright__ = "Copyright 2017, Xilinx"
-__version__ = 1.1
+__copyright__ = "Copyright 2018, Xilinx"
+__version__ = 0.1
 
 
 
